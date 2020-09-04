11a35116216d7d56cdb931435c2ea806bad2aeb5
Sebastijan Kocen <62952614+skocen@users.noreply.github.com>
2020-09-04 06:42:14 +0200

    Add files via upload

---
+++ euclid.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int euclid (int x, int y)
+{
+    int z,w,k;
+    if (x>y)
+        swap(x,y);
+    k=x;
+    w=y;
+    while(z!=0)
+    {
+        z=k%w;
+        k=w;
+        w=z;
+    }
+return k;
+}
+int main()
+{
+    int x,y;
+    cout<<"Enter first number:";
+    cin>>x;
+    cout<<"Enter second number:";
+    cin>>y;
+    cout<<"Greatest common divisor: "<<euclid(x,y);
+    return 0;
+}
